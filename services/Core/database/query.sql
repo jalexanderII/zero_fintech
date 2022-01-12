6163b6c73270ef8a051530d88ecd34172f6f67cb
jalexanderII <a.joeljr@hotmail.com>
2022-01-11 19:53:26 -0500

    add create methods to coredb table

--- services/Core/database/query.sql
+++ services/Core/database/query.sql
@@ -7,3 +7,48 @@ VALUES ($1,
         $2,
         $3,
         $4) RETURNING *;
+
+-- name: CreateAccount :one
+INSERT INTO accounts (user_id,
+                      name,
+                      annual_percentage_rate,
+                      penalty_apr,
+                      due_day,
+                      minimum_interest_charge,
+                      annual_account_fee,
+                      foreign_transaction_fee,
+                      promotional_rate,
+                      minimum_payment_due,
+                      current_balance,
+                      pending_transactions,
+                      credit_limit)
+VALUES ($1,
+        $2,
+        $3,
+        $4,
+        $5,
+        $6,
+        $7,
+        $8,
+        $9,
+        $10,
+        $11,
+        $12,
+        $13) RETURNING *;
+
+
+-- name: CreateTransaction :one
+INSERT INTO transactions (user_id,
+                          account_id,
+                          name,
+                          amount,
+                          date,
+                          rewards_earned,
+                          transaction_details)
+VALUES ($1,
+        $2,
+        $3,
+        $4,
+        $5,
+        $6,
+        $7) RETURNING *;
